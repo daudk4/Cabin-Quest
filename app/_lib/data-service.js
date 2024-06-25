d1e553c4edf28a70a5bbaacaab3bc2d96506d146
daud khan <daudu6944@gmail.com>
2024-06-25 11:22:27 +0500

    reservation form

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -133,6 +133,9 @@ export async function getSettings() {
     throw new Error("Settings could not be loaded");
   }
 
+  // For testing
+  await new Promise((res) => setTimeout(res, 5000));
+
   return data;
 }
 
