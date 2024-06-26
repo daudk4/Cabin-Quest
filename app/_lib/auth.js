38db1015ac57578c819b08feab133264f6f67e32
daud khan <daudu6944@gmail.com>
2024-06-26 11:52:50 +0500

    add GitHub Auth

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,11 +1,23 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
 
 const authCOnfig = {
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
     }),
   ],
 };
